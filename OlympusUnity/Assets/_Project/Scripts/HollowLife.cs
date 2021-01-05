a7cc6fc95914f267bdd04528c2657858c790c965
Ricky <hollowtaylor@gmail.com>
2021-01-05 12:07:54 +0000

    script change test

--- OlympusUnity/Assets/_Project/Scripts/HollowLife.cs
+++ OlympusUnity/Assets/_Project/Scripts/HollowLife.cs
@@ -7,7 +7,7 @@ public class HollowLife : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // mod test
     }
 
     // Update is called once per frame
