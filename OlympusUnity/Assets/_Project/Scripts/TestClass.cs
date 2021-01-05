19abed2b4470dd82d7efb564ce5bc13c5ab3ebfc
TheNiallMc1 <niallmccoy123@outlook.com>
2021-01-05 10:32:18 +0000

    Added test script

---
+++ OlympusUnity/Assets/_Project/Scripts/TestClass.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestClass : MonoBehaviour
+{
+    // This is for testing github commits
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
