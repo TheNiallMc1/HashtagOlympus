41801473b97fbde0677324909eef8f8f7a595325
Niall McCoy <niallmccoy123@outlook.com>
2021-01-05 12:13:15 +0000

    added comment to MatTest

--- OlympusUnity/Assets/_Project/Scripts/MatTest.cs
+++ OlympusUnity/Assets/_Project/Scripts/MatTest.cs
@@ -9,7 +9,7 @@ public class MatTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Niall's comment :3c
     }
 
     // Update is called once per frame
