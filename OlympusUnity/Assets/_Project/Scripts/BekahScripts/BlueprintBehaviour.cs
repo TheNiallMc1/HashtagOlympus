b258d5d21dd8e7a9d45de5f02e1772838197b7c3
bekah <caffeine@hotmail.co.uk>
2021-01-07 17:25:29 +0000

    fixed destroyed object bug

--- OlympusUnity/Assets/_Project/Scripts/BekahScripts/BlueprintBehaviour.cs
+++ OlympusUnity/Assets/_Project/Scripts/BekahScripts/BlueprintBehaviour.cs
@@ -37,23 +37,28 @@ public class BlueprintBehaviour : MonoBehaviour
 
     private void Update()
     {
-        Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
-        if (Physics.Raycast(ray, out _hit,50000.0f,(1<<8)))
-        {
-            _newPosition = new Vector3(_hit.point.x, 0f,_hit.point.z);
-            transform.position =_newPosition;
-        }
+            Ray ray = _cam.ScreenPointToRay(Mouse.current.position.ReadValue());
+            if (Physics.Raycast(ray, out _hit, 50000.0f, (1 << 8)))
+            {
+                _newPosition = new Vector3(_hit.point.x, 0f, _hit.point.z);
+                transform.position = _newPosition;
+            }
+        
     }
 
     public void SetDown()
     {
+        
         _newPosition.y = 0;
             Debug.Log("Instantiating ");
             //Instantiate(prefab, _hit.point, transform.rotation);
             prefab.transform.position = _newPosition;
-
+            
+          
             if (gameObject != null)
             {
+                _playerControls.Player.MouseClick.performed -= ctx => SetDown();
+                _playerControls.Disable();
                 Destroy(gameObject);
             }
     }
