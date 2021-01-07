7a6c2cea9c9ab5f603279e273c023b8b563e1966
Niall McCoy <niallmccoy123@outlook.com>
2021-01-07 11:16:30 +0000

    Added GameManager, added TouristBehaviour

---
+++ OlympusUnity/Assets/_Project/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance => _instance;
+    
+    public GodBehaviour currentlySelectedGod;
+
+    private void Awake()
+    {
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+}
---
+++ OlympusUnity/Assets/_Project/Scripts/TouristBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouristBehaviour : MonoBehaviour
+{
+    [Header("Character Stats")]
+    public int maxHealth;
+    protected int currentHealth;
+    protected int attackDamage;
+    
+    [Header("AI Weights")]
+    public int attackGodChance;
+    public int attackStructureChance;
+
+    [Header("Waypoint Scoring")]
+    //public Waypoint currentWaypoint;
+    
+    public int closestToFinalWaypoint;
+
+    public int godMonument;
+    public int touristMonument;
+    
+    public int godsNearby;
+    public int touristsNearby;
+}
