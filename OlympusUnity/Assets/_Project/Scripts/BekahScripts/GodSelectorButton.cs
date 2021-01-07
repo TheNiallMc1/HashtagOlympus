11fe42e80d15454a9a2f0c3e2e83de6ea37cbb76
bekah <caffeine@hotmail.co.uk>
2021-01-07 14:27:36 +0000

    Merge branch 'main' of https://github.com/TheNiallMc1/HashtagOlympus into main

---
+++ OlympusUnity/Assets/_Project/Scripts/BekahScripts/GodSelectorButton.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GodSelectorButton : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject selectedGod;
+    
+    private Button btn;
+
+    void Start ( )
+    {
+        btn = GetComponent<Button> ( );
+        
+    }
+    
+    public void SendSelection()
+    {
+       
+        BekahsGM.Instance.AddGodToActiveList(this.selectedGod);
+        BekahsGM.Instance.PassedGameObject = selectedGod;
+        btn.gameObject.SetActive(false);
+    }
+}
