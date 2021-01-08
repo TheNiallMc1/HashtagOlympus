3a0a345cf3e5a86ecb795547a8fc2360254e03db
Ricky <hollowtaylor@gmail.com>
2021-01-08 15:10:57 +0000

    --

--- OlympusUnity/Assets/_Project/Scripts/BekahsGM.cs
+++ OlympusUnity/Assets/_Project/Scripts/BekahsGM.cs
@@ -4,85 +4,85 @@ using UnityEngine;
 
 public class BekahsGM : MonoBehaviour
 {
-    public int totalRespect;
+    //public int totalRespect;
 
     
+    ////public int activeGodCount;
+    ////public Canvas selectorScreen;
+
     //public int activeGodCount;
     //public Canvas selectorScreen;
 
-    public int activeGodCount;
-    public Canvas selectorScreen;
-
     
-    private static BekahsGM _instance = null; // the private static singleton instance variable
-    public static BekahsGM Instance { get { return _instance; } } // public getter property, anyone can access it!
+    //private static BekahsGM _instance = null; // the private static singleton instance variable
+    //public static BekahsGM Instance { get { return _instance; } } // public getter property, anyone can access it!
 
-    private void Awake()
-    {
-        if (_instance == null)
-        {
-            // if the singleton instance has not yet been initialized
-            _instance = this;
-        }
-        else
-        {
-            // the singleton instance has already been initialized
-            if (_instance != this)
-            {
-                // if this instance of GameManager is not the same as the initialized singleton instance, it is a second instance, so it must be destroyed!
-                Destroy(gameObject); // watch out, this can cause trouble!
-            }
-        }
-    }
+    //private void Awake()
+    //{
+    //    if (_instance == null)
+    //    {
+    //        // if the singleton instance has not yet been initialized
+    //        _instance = this;
+    //    }
+    //    else
+    //    {
+    //        // the singleton instance has already been initialized
+    //        if (_instance != this)
+    //        {
+    //            // if this instance of GameManager is not the same as the initialized singleton instance, it is a second instance, so it must be destroyed!
+    //            Destroy(gameObject); // watch out, this can cause trouble!
+    //        }
+    //    }
+    //}
 
-    private GameObject _passedGameObject;
-    public GameObject PassedGameObject
-    {
-        get => _passedGameObject;
-        set
-        {
-            _passedGameObject = value;
-            Debug.Log ( $"Receiver[{name}] just received \'{_passedGameObject.name}\'" );
-        }
-    }
-
-
-    void OnDestroy()
-    {
-        if (_instance == this)
-        {
-            // if the this is the singleton instance that is being destroyed...
-            _instance = null; // set the instance to null
-        }
-    }
+    //private GameObject _passedGameObject;
+    //public GameObject PassedGameObject
+    //{
+    //    get => _passedGameObject;
+    //    set
+    //    {
+    //        _passedGameObject = value;
+    //        Debug.Log ( $"Receiver[{name}] just received \'{_passedGameObject.name}\'" );
+    //    }
+    //}
 
 
-    public List<GameObject> currentGods;
-    // Start is called before the first frame update
-    void Start()
-    {
+    //void OnDestroy()
+    //{
+    //    if (_instance == this)
+    //    {
+    //        // if the this is the singleton instance that is being destroyed...
+    //        _instance = null; // set the instance to null
+    //    }
+    //}
+
+
+    //public List<GameObject> currentGods;
+    //// Start is called before the first frame update
+    //void Start()
+    //{
         
-    }
+    //}
 
-    // Update is called once per frame
-    void Update()
-    {
+    //// Update is called once per frame
+    //void Update()
+    //{
         
-    }
+    //}
 
-    public void AddGodToActiveList(GameObject selectedGod)
-    {
-        currentGods.Add(selectedGod);
+    //public void AddGodToActiveList(GameObject selectedGod)
+    //{
+    //    currentGods.Add(selectedGod);
 
-        Debug.Log("Added "+selectedGod.GetComponentInChildren<GodBehaviour>().godName+" to god list");
-        activeGodCount++;
+    //    Debug.Log("Added "+selectedGod.GetComponentInChildren<GodBehaviour>().godName+" to god list");
+    //    activeGodCount++;
 
-        if (activeGodCount == 3)
-        {
-            selectorScreen.gameObject.SetActive(false);
-        }
+    //    if (activeGodCount == 3)
+    //    {
+    //        selectorScreen.gameObject.SetActive(false);
+    //    }
 
-    }
+    //}
     
     
 }
--- OlympusUnity/Assets/_Project/Scripts/GameManager.cs
+++ OlympusUnity/Assets/_Project/Scripts/GameManager.cs
@@ -5,25 +5,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    private static GameManager _instance;
-    public static GameManager Instance => _instance;
+    //private static GameManager _instance;
+    //public static GameManager Instance => _instance;
     
-    public GodBehaviour currentlySelectedGod;
+    //public GodBehaviour currentlySelectedGod;
 
-    private void Awake()
-    {
-        if (_instance != null && _instance != this)
-        {
-            Destroy(this.gameObject);
-        }
-        else
-        {
-            _instance = this;
-        }
-    }
+    //private void Awake()
+    //{
+    //    if (_instance != null && _instance != this)
+    //    {
+    //        Destroy(this.gameObject);
+    //    }
+    //    else
+    //    {
+    //        _instance = this;
+    //    }
+    //}
 
-    private void SelectGod(GodBehaviour godToSelect)
-    {
-        print(godToSelect.gameObject.name);
-    }
+    //private void SelectGod(GodBehaviour godToSelect)
+    //{
+    //    print(godToSelect.gameObject.name);
+    //}
 }
