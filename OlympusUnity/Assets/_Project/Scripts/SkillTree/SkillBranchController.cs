060bd35dc3f1fa238cf293a294068e15bb3216e3
Niall McCoy <niallmccoy123@outlook.com>
2021-01-14 17:26:33 +0000

    Skills now generate in the UI

--- OlympusUnity/Assets/_Project/Scripts/SkillTree/SkillBranchController.cs
+++ OlympusUnity/Assets/_Project/Scripts/SkillTree/SkillBranchController.cs
@@ -15,6 +15,7 @@ public class SkillBranchController : MonoBehaviour
     
     [Header("UI Elements")] 
     public TextMeshProUGUI branchNameText;
+    public GameObject skillsContainer;
     
     // Local versions of the SO variables, to avoid fetching them everytime we need them
     private string branchName;
@@ -26,5 +27,20 @@ public class SkillBranchController : MonoBehaviour
         branchName = branchInfo.branchName;
 
         branchNameText.text = branchName;
+        
+        GenerateSkillNodes();
+    }
+
+    public void GenerateSkillNodes()
+    {
+        foreach (Skill skillInfo in skillList)
+        {
+            // Instantiate a new skill node and access its script
+            GameObject skillObject = Instantiate(skillNodePrefab, skillsContainer.transform);
+            SkillNode skillScript = skillObject.GetComponent<SkillNode>();
+
+            // Set the SO reference in this new skill to the Skill asset at this index
+            skillScript.skillInfo = skillInfo;
+        }
     }
 }
--- OlympusUnity/Assets/_Project/Scripts/SkillTree/SkillsManager.cs
+++ OlympusUnity/Assets/_Project/Scripts/SkillTree/SkillsManager.cs
@@ -1,18 +1,39 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SkillsManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    // Information on which character's skill set this is
+    public CharacterSkillSet skillSetInfo;
+    
+    // The branch list from the SO, stored locally
+    private List<SkillBranch> branchList;
+
+    public GameObject skillBranchPrefab;
+
+    [Header("UI Elements")] 
+    public GameObject skillBranchContainer;
+
+    public void Start()
     {
+        branchList = skillSetInfo.skillBranches;
         
+        GenerateBranches();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GenerateBranches()
     {
-        
+        foreach (SkillBranch branchInfo in branchList)
+        {
+            // Instantiate a new skill branch and access its script
+            GameObject branchObject = Instantiate(skillBranchPrefab, skillBranchContainer.transform);
+            SkillBranchController branchScript = branchObject.GetComponent<SkillBranchController>();
+
+            // Set the SO reference in this new branch to the SkillBranch asset at this index
+            branchScript.branchInfo = branchInfo;
+        }
     }
 }
