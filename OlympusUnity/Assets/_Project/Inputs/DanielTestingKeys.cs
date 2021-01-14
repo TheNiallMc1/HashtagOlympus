fc6414c6af7e4f8a531c6edda625c9663e1ee2b6
Daniel Gould <danfgould@gmail.com>
2021-01-14 18:02:21 +0000

    Single-Target functionality mostly in place
    Need to add graphical assist method to differentiate targets

--- OlympusUnity/Assets/_Project/Inputs/DanielTestingKeys.cs
+++ OlympusUnity/Assets/_Project/Inputs/DanielTestingKeys.cs
@@ -41,6 +41,30 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
                     ""expectedControlType"": ""Button"",
                     ""processors"": """",
                     ""interactions"": """"
+                },
+                {
+                    ""name"": ""LeftClick"",
+                    ""type"": ""Button"",
+                    ""id"": ""749325f1-b293-404b-adcf-7d59aafea3e5"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""RightClick"",
+                    ""type"": ""Button"",
+                    ""id"": ""4724234a-040e-4719-8408-7458270a4023"",
+                    ""expectedControlType"": ""Button"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MousePosition"",
+                    ""type"": ""Value"",
+                    ""id"": ""76b33137-5478-4262-8722-cc2a0391daed"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
                 }
             ],
             ""bindings"": [
@@ -76,6 +100,39 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
                     ""action"": ""TestKey3"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""3b19f4a9-0f67-4a7e-937e-990d6b7d7cc3"",
+                    ""path"": ""<Mouse>/leftButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""LeftClick"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""5911934a-0461-4098-b185-14c4d8db1ed9"",
+                    ""path"": ""<Mouse>/rightButton"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""RightClick"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""a3fdbbfc-822c-46b4-8d46-f9eec0bc7a80"",
+                    ""path"": ""<Mouse>/position"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""MousePosition"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -87,6 +144,9 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
         m_TestKeys_TestKey1 = m_TestKeys.FindAction("TestKey1", throwIfNotFound: true);
         m_TestKeys_TestKey2 = m_TestKeys.FindAction("TestKey2", throwIfNotFound: true);
         m_TestKeys_TestKey3 = m_TestKeys.FindAction("TestKey3", throwIfNotFound: true);
+        m_TestKeys_LeftClick = m_TestKeys.FindAction("LeftClick", throwIfNotFound: true);
+        m_TestKeys_RightClick = m_TestKeys.FindAction("RightClick", throwIfNotFound: true);
+        m_TestKeys_MousePosition = m_TestKeys.FindAction("MousePosition", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -139,6 +199,9 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
     private readonly InputAction m_TestKeys_TestKey1;
     private readonly InputAction m_TestKeys_TestKey2;
     private readonly InputAction m_TestKeys_TestKey3;
+    private readonly InputAction m_TestKeys_LeftClick;
+    private readonly InputAction m_TestKeys_RightClick;
+    private readonly InputAction m_TestKeys_MousePosition;
     public struct TestKeysActions
     {
         private @DanielTestingKeys m_Wrapper;
@@ -146,6 +209,9 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
         public InputAction @TestKey1 => m_Wrapper.m_TestKeys_TestKey1;
         public InputAction @TestKey2 => m_Wrapper.m_TestKeys_TestKey2;
         public InputAction @TestKey3 => m_Wrapper.m_TestKeys_TestKey3;
+        public InputAction @LeftClick => m_Wrapper.m_TestKeys_LeftClick;
+        public InputAction @RightClick => m_Wrapper.m_TestKeys_RightClick;
+        public InputAction @MousePosition => m_Wrapper.m_TestKeys_MousePosition;
         public InputActionMap Get() { return m_Wrapper.m_TestKeys; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -164,6 +230,15 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
                 @TestKey3.started -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnTestKey3;
                 @TestKey3.performed -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnTestKey3;
                 @TestKey3.canceled -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnTestKey3;
+                @LeftClick.started -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnLeftClick;
+                @LeftClick.performed -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnLeftClick;
+                @LeftClick.canceled -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnLeftClick;
+                @RightClick.started -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnRightClick;
+                @RightClick.performed -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnRightClick;
+                @RightClick.canceled -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnRightClick;
+                @MousePosition.started -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnMousePosition;
+                @MousePosition.performed -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnMousePosition;
+                @MousePosition.canceled -= m_Wrapper.m_TestKeysActionsCallbackInterface.OnMousePosition;
             }
             m_Wrapper.m_TestKeysActionsCallbackInterface = instance;
             if (instance != null)
@@ -177,6 +252,15 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
                 @TestKey3.started += instance.OnTestKey3;
                 @TestKey3.performed += instance.OnTestKey3;
                 @TestKey3.canceled += instance.OnTestKey3;
+                @LeftClick.started += instance.OnLeftClick;
+                @LeftClick.performed += instance.OnLeftClick;
+                @LeftClick.canceled += instance.OnLeftClick;
+                @RightClick.started += instance.OnRightClick;
+                @RightClick.performed += instance.OnRightClick;
+                @RightClick.canceled += instance.OnRightClick;
+                @MousePosition.started += instance.OnMousePosition;
+                @MousePosition.performed += instance.OnMousePosition;
+                @MousePosition.canceled += instance.OnMousePosition;
             }
         }
     }
@@ -186,5 +270,8 @@ public class @DanielTestingKeys : IInputActionCollection, IDisposable
         void OnTestKey1(InputAction.CallbackContext context);
         void OnTestKey2(InputAction.CallbackContext context);
         void OnTestKey3(InputAction.CallbackContext context);
+        void OnLeftClick(InputAction.CallbackContext context);
+        void OnRightClick(InputAction.CallbackContext context);
+        void OnMousePosition(InputAction.CallbackContext context);
     }
 }
--- OlympusUnity/Assets/_Project/Scripts/Gods/DanielPlayerScript.cs
+++ OlympusUnity/Assets/_Project/Scripts/Gods/DanielPlayerScript.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,6 +12,15 @@ public class DanielPlayerScript : MonoBehaviour
     private bool key2;
     private bool key3;
 
+    public bool leftClick;
+    public bool rightClick;
+
+    private Vector2 mousePosition;
+
+    public bool selectionModeActive = false;
+
+    public Combatant.TargetType abilityHitCheck;
+
     private void Awake()
     {
         testKeys = new DanielTestingKeys();
@@ -19,10 +29,16 @@ public class DanielPlayerScript : MonoBehaviour
         testKeys.TestKeys.TestKey1.started += ctx => key1 = true;
         testKeys.TestKeys.TestKey2.started += ctx => key2 = true;
         testKeys.TestKeys.TestKey3.started += ctx => key3 = true;
+        testKeys.TestKeys.LeftClick.started += ctx => leftClick = true;
+        testKeys.TestKeys.RightClick.started += ctx => rightClick = true;
+
+        testKeys.TestKeys.MousePosition.performed += ctx => mousePosition = ctx.ReadValue<Vector2>();
 
         testKeys.TestKeys.TestKey1.canceled -= ctx => key1 = false;
         testKeys.TestKeys.TestKey2.canceled -= ctx => key2 = false;
         testKeys.TestKeys.TestKey3.canceled -= ctx => key3 = false;
+        testKeys.TestKeys.LeftClick.canceled -= ctx => leftClick = false;
+        testKeys.TestKeys.RightClick.canceled -= ctx => rightClick = false;
     }
 
     // Start is called before the first frame update
@@ -34,10 +50,47 @@ public class DanielPlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (key3)
+        if (selectionModeActive)
         {
-            key3 = false;
-            ConeAoE coneAoE = Instantiate(coneAoePrefab, transform.position, Quaternion.identity, transform);
+            GraphicAssist(abilityHitCheck);
+
+
+            if (rightClick)
+            {
+                leftClick = false;
+                rightClick = false;
+
+                Ray ray = Camera.main.ScreenPointToRay(mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit, 100))
+                {
+                    Combatant combatant = hit.transform.gameObject.GetComponent<Combatant>();
+
+                    if (combatant != null && combatant.targetType == abilityHitCheck)
+                    {
+                        Debug.Log("Attacked " + hit.transform.gameObject.name);
+                        selectionModeActive = false;
+                    }
+                }
+                
+            }
+            
         }
+
+    }
+
+    private void GraphicAssist(Combatant.TargetType targetType)
+    {
+        // Makes things with this TargetType stand out visually
+    }
+
+    public void EnterSelectionMode()
+    {
+        leftClick = false;
+        rightClick = false;
+        selectionModeActive = true;
+
+        print("Selection mode activated");
     }
 }
