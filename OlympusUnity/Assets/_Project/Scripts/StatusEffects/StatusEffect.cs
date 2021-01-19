b1521927ccaf78bee464d5bd862b3d5fd7bd66c8
Niall McCoy <niallmccoy123@outlook.com>
2021-01-19 17:32:16 +0000

    Commit before merge into main

--- OlympusUnity/Assets/_Project/Scripts/StatusEffects/StatusEffect.cs
+++ OlympusUnity/Assets/_Project/Scripts/StatusEffects/StatusEffect.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public abstract class StatusEffect : ScriptableObject
 {
+    [Header("Priority")] 
+    [Tooltip("Effects with a higher priority will perform their logic before those with lower ones")]
+    public int effectPriority;
+    
     [Header("Duration")]
     [SerializeField] [Tooltip("Set to zero to make an instant cast status, like Heal")]
     public float statusDuration;
@@ -12,12 +16,11 @@ public abstract class StatusEffect : ScriptableObject
     [Header("Tick Behaviour")] 
     public bool isTickType;
     public float tickInterval;
-
     
     [HideInInspector] public Combatant inflictedBy; // The combatant who applied this effect    
     [HideInInspector] public Combatant affectedCombatant; // The combatant this effect is applied to
     
-    public abstract void TickEffect(); // Apples once every x seconds as determined
+    public abstract void TickEffect(); // Applies once every x seconds as determined
     public abstract void EntryEffect(); // Applies when the effect is applied
     public abstract void ExitEffect(); // Applies when the effect is removed
 }
