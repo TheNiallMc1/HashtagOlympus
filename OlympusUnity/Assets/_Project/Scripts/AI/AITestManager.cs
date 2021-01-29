ae81ef28d8afb71dcdd69167b8563734ceee2692
Ricky <hollowtaylor@gmail.com>
2021-01-29 10:27:43 +0000

    changes to pool and spawner

--- OlympusUnity/Assets/_Project/Scripts/AI/AITestManager.cs
+++ OlympusUnity/Assets/_Project/Scripts/AI/AITestManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class AITestManager : MonoBehaviour
 {
@@ -16,7 +17,7 @@ public class AITestManager : MonoBehaviour
     public float countDown = 2f;
 
     [SerializeField]
-    private List<Transform> touristDrones;
+    private List<Transform> touristList;
     
     public int enemiesPerWave = 0;
     public int numberOfWaves = 3;
@@ -53,15 +54,35 @@ public class AITestManager : MonoBehaviour
 
     private bool TouristsIsAlive()
     {
-        touristDrones = touristDrones.Where(e => e != null).ToList();
+        touristList = touristList.Where(e => e != null).ToList();
 
-        return touristDrones.Count > 0;
+        return touristList.Count > 0;
     }
 
     private IEnumerator SpawnWave()
     {
         for (var i = 0; i < enemiesPerWave; i++)
         {
+            if (i % 5 == 0)
+            {
+                SpawnJock(i);
+                yield return new WaitForSeconds(0.5f);
+                continue;
+            }
+            if (i % 3 == 0)
+            {
+                SpawnNerd(i);
+                yield return new WaitForSeconds(0.5f);
+                continue;
+            }
+
+            if (i % 6 == 0)
+            {
+                SpawnInfluencer(i);
+                yield return new WaitForSeconds(0.5f);
+                continue;
+            }
+
             SpawnTourist(i);
             yield return new WaitForSeconds(0.5f);
         }
@@ -70,13 +91,52 @@ public class AITestManager : MonoBehaviour
 
     private void SpawnTourist(int nameTestInt)
     {
-        GameObject touristDrone = ObjectPools.SharedInstance.GetPoolObGameObject();
-        if (touristDrone != null)
+        GameObject tourist = ObjectPools.SharedInstance.GetDronePoolObGameObject();
+        if (tourist != null)
         {
-            touristDrone.transform.position = left.position;
-            touristDrone.transform.rotation = left.rotation;
-            touristDrone.SetActive(true);
-            touristDrones.Add(touristDrone.transform);
+            tourist.transform.position = left.position;
+            tourist.transform.rotation = left.rotation;
+            tourist.SetActive(true);
+            touristList.Add(tourist.transform);
+        }
+
+    }
+
+    private void SpawnInfluencer(int nameTestInt)
+    {
+        GameObject tourist = ObjectPools.SharedInstance.GetInfluencerPoolObGameObject();
+        if (tourist != null)
+        {
+            tourist.transform.position = left.position;
+            tourist.transform.rotation = left.rotation;
+            tourist.SetActive(true);
+            touristList.Add(tourist.transform);
+        }
+
+    }
+
+    private void SpawnJock(int nameTestInt)
+    {
+        GameObject tourist = ObjectPools.SharedInstance.GetJockPoolObGameObject();
+        if (tourist != null)
+        {
+            tourist.transform.position = left.position;
+            tourist.transform.rotation = left.rotation;
+            tourist.SetActive(true);
+            touristList.Add(tourist.transform);
+        }
+
+    }
+
+    private void SpawnNerd(int nameTestInt)
+    {
+        GameObject tourist = ObjectPools.SharedInstance.GetNerdPoolObGameObject();
+        if (tourist != null)
+        {
+            tourist.transform.position = left.position;
+            tourist.transform.rotation = left.rotation;
+            tourist.SetActive(true);
+            touristList.Add(tourist.transform);
         }
 
     }
--- OlympusUnity/Assets/_Project/Scripts/ObjectPools.cs
+++ OlympusUnity/Assets/_Project/Scripts/ObjectPools.cs
@@ -1,11 +1,18 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class ObjectPools : MonoBehaviour
 {
     public static ObjectPools SharedInstance;
     public List<GameObject> pooledTouristDrones;
-    public GameObject objectToPool;
+    public List<GameObject> pooledTouristInfluencer;
+    public List<GameObject> pooledTouristJock;
+    public List<GameObject> pooledTouristNerd;
+    public GameObject droneObjectToPool;
+    public GameObject influencerObjectToPool;
+    public GameObject jockObjectToPool;
+    public GameObject nerdObjectToPool;
     public int amountToPool;
 
     // Start is called before the first frame update
@@ -21,19 +28,70 @@ public class ObjectPools : MonoBehaviour
         GameObject tmp;
         for (var i = 0; i < amountToPool; i++)
         {
-            tmp = Instantiate(objectToPool);
+            tmp = Instantiate(droneObjectToPool);
             tmp.SetActive(false);
             pooledTouristDrones.Add(tmp);
         }
+        for (var i = 0; i < 10; i++)
+        {
+            tmp = Instantiate(influencerObjectToPool);
+            tmp.SetActive(false);
+            pooledTouristInfluencer.Add(tmp);
+        }
+        for (var i = 0; i < 10; i++)
+        {
+            tmp = Instantiate(jockObjectToPool);
+            tmp.SetActive(false);
+            pooledTouristJock.Add(tmp);
+        }
+        for (var i = 0; i < 10; i++)
+        {
+            tmp = Instantiate(nerdObjectToPool);
+            tmp.SetActive(false);
+            pooledTouristNerd.Add(tmp);
+        }
     }
 
-    public GameObject GetPoolObGameObject()
+    public GameObject GetDronePoolObGameObject()
     {
         for (var i = 0; i < amountToPool; i++)
         {
             if(!pooledTouristDrones[i].activeInHierarchy)
             {
                 return pooledTouristDrones[i];
+            }
+        }
+        return null;
+    }
+    public GameObject GetInfluencerPoolObGameObject()
+    {
+        for (var i = 0; i < amountToPool; i++)
+        {
+            if (!pooledTouristInfluencer[i].activeInHierarchy)
+            {
+                return pooledTouristInfluencer[i];
+            }
+        }
+        return null;
+    }
+    public GameObject GetJockPoolObGameObject()
+    {
+        for (var i = 0; i < amountToPool; i++)
+        {
+            if (!pooledTouristJock[i].activeInHierarchy)
+            {
+                return pooledTouristJock[i];
+            }
+        }
+        return null;
+    }
+    public GameObject GetNerdPoolObGameObject()
+    {
+        for (var i = 0; i < amountToPool; i++)
+        {
+            if (!pooledTouristNerd[i].activeInHierarchy)
+            {
+                return pooledTouristNerd[i];
             }
         }
         return null;
