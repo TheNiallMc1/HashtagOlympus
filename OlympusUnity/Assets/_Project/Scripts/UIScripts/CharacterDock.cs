cd5eeb28dd6be8357dbf87df028b6ecdffa5568b
bekah <caffeine@hotmail.co.uk>
2021-01-29 10:07:16 +0000

    dock tool tips somewhat working, character sprites imported

--- OlympusUnity/Assets/_Project/Scripts/UIScripts/CharacterDock.cs
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/CharacterDock.cs
@@ -12,6 +12,8 @@ public class CharacterDock : MonoBehaviour
     
     public Combatant godCombatant;
     private GodBehaviour godBehaviour;
+    private CharacterToolTipInfo tooltipInfo;
+    
    // public RespectBuff respectBuff;
     public TMP_Text godNameDisplay;
     public TMP_Text godHealthDisplay;
@@ -21,6 +23,9 @@ public class CharacterDock : MonoBehaviour
     public HealthBar healthBar;
     public RespectBuff respectBuff;
 
+    public List<TooltipTrigger> abilityTooltips;
+    public GameObject abilityButtons;
+
     private void Awake()
     {
         reviveButton.gameObject.SetActive(false);
@@ -29,9 +34,23 @@ public class CharacterDock : MonoBehaviour
 
     public void UpdateCharacterDock()
     {
+        tooltipInfo = godCombatant.gameObject.GetComponent<CharacterToolTipInfo>();
         godNameDisplay.text = godCombatant.characterName;
         godHealthDisplay.text = godCombatant.currentHealth + "/" + godCombatant.maxHealth;
         healthBar.healthValue = godCombatant.currentHealth;
+
+        /*foreach (var tooltip in abilityTooltips)
+        {
+            for (int i = 0; i < 11; i++)
+            {
+                tooltip.header = tooltipInfo.allTooltips[i];
+                i++;
+                tooltip.content = tooltipInfo.allTooltips[i];
+                i++;
+            }
+        }*/
+
+      UpdateTooltips();
 
         if (godBehaviour.isKOed)
         {
@@ -47,6 +66,7 @@ public class CharacterDock : MonoBehaviour
         godNameDisplay.text = godCombatant.characterName;
         godHealthDisplay.text = godCombatant.currentHealth + "/" + godCombatant.maxHealth;
         healthBar.healthValue = godCombatant.currentHealth;
+        //UpdateTooltips();
 
         if (godBehaviour.isKOed)
         {
@@ -54,15 +74,35 @@ public class CharacterDock : MonoBehaviour
         }
         //adding correct ability buttons
     }
+
+    public void UpdateTooltips()
+    {
+        abilityTooltips[0].header = tooltipInfo.allTooltips[0];
+        abilityTooltips[0].content = tooltipInfo.allTooltips[1];
+        
+        abilityTooltips[1].header = tooltipInfo.allTooltips[2];
+        abilityTooltips[1].content = tooltipInfo.allTooltips[3];
+        
+        abilityTooltips[2].header = tooltipInfo.allTooltips[4];
+        abilityTooltips[2].content = tooltipInfo.allTooltips[5];
+        
+        abilityTooltips[3].header = tooltipInfo.allTooltips[6];
+        abilityTooltips[3].content = tooltipInfo.allTooltips[7];
+        
+        abilityTooltips[4].header = tooltipInfo.allTooltips[8];
+        abilityTooltips[4].content = tooltipInfo.allTooltips[9];
+    }
     
     void ShowReviveButton()
     {
+        abilityButtons.gameObject.SetActive(false);
         reviveButton.gameObject.SetActive(true);
     }
     
     public void ReviveButton()
     {
         godBehaviour.Revive();
+        abilityButtons.gameObject.SetActive(true);
         reviveButton.gameObject.SetActive(false);
         GameManager.Instance.RemoveRespect(godBehaviour.costToRespawn);
         Debug.Log("revive pressed");
---
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/CharacterToolTipInfo.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterToolTipInfo : MonoBehaviour
+{
+
+    [Header("Auto Attack")]
+    public string autoAttackHeader = "Auto Attack";
+    public string autoAttackContent = "...";
+    
+    [Header("Abiity 1")]
+    public string ability1Header = "Ability 1";
+    public string ability1Content = "...";
+    
+    [Header("Ability2")]
+    public string ability2Header = "Ability 2";
+    public string ability2Content = "...";
+    
+    [Header("Passive")]
+    public string passiveHeader = "Passive";
+    public string passiveContent = "...";
+    
+    [Header("Ultimate")]
+    public string ultimateHeader = "Ultimate";
+    public string ultimateContent = "...";
+
+    [SerializeField]
+    public List<String> allTooltips;
+
+    private void Awake()
+    {
+       allTooltips.Add(autoAttackHeader);
+       allTooltips.Add(autoAttackContent);
+       
+       allTooltips.Add(ability1Header);
+       allTooltips.Add(ability1Content);
+       
+       allTooltips.Add(ability2Header);
+       allTooltips.Add(ability2Content);
+       
+       allTooltips.Add(passiveHeader);
+       allTooltips.Add(passiveContent);
+       
+       allTooltips.Add(ultimateHeader);
+       allTooltips.Add(ultimateContent);
+       
+       Debug.Log("ttoltips count : "+allTooltips.Count);
+    }
+}
