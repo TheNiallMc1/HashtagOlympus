b1f0f7c148b5dc6a7b2b0e64826842e37b3de4c3
Niall McCoy <niallmccoy123@outlook.com>
2021-02-04 20:28:43 +0000

    Health bar fixed!!!! 😁😍
    float not int

--- OlympusUnity/Assets/_Project/Scripts/UIScripts/HealthBar.cs
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/HealthBar.cs
@@ -13,17 +13,14 @@ public class HealthBar : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-         
+        
     }
 
     public void UpdateHealthBar(float healthV)
     {
-        print("1Health of " + this.gameObject.name + " is " + healthV);    
         float amount = (healthV / 100f) * 180f / 360;
         bar.fillAmount = amount;
-        print("Amount of " + this.gameObject.name + " is " + amount);
         float buttonAngle = amount * 360;
-        print("ButtonAngle of " + this.gameObject.name + " is " + buttonAngle);
         button.localEulerAngles = new Vector3(0,0,-buttonAngle);
     }
 }
