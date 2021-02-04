25981c1c1cccd69d764e04ee67ad88bdf9614b29
bekah <caffeine@hotmail.co.uk>
2021-02-04 12:26:07 +0000

    Started implementing tutorial

---
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/Tutorial/TutorialManager.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    private static TutorialManager _instance;
+    public static TutorialManager Instance => _instance;
+
+    public TutorialText tutText;
+    
+    [SerializeField]
+    public List<GameObject> allTutorials;
+
+    [SerializeField] public List<GameObject> pauseModeTutorials;
+    
+    [SerializeField] public List<GameObject> infoButtons;
+    
+    public int tutorialIndex;
+    public int pauseTutIndex;
+
+    public bool mainTutorialComplete = false;
+
+    private void Awake()
+    {
+        // Creating singleton
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        else
+        {
+            _instance = this;
+        }
+    }
+
+    public void Start()
+    {
+        foreach (var tutorial in allTutorials)
+        {
+            tutorial.gameObject.SetActive(false);
+        }
+        allTutorials[tutorialIndex].SetActive(true);
+    }
+
+    public void IncrementTutorialIndex()
+    {
+        tutorialIndex++;
+        if (tutorialIndex >= allTutorials.Count)
+        {
+            mainTutorialComplete = true;
+            tutorialIndex = 0;
+        }
+        
+        if (!mainTutorialComplete)
+        {
+            allTutorials[tutorialIndex].gameObject.SetActive(true);
+        }
+    }
+
+    public void DisplaySpecificTutorial(int indexToDisplay)
+    {
+        allTutorials[indexToDisplay].gameObject.SetActive(true);
+    }
+}
---
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/Tutorial/TutorialText.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using TMPro;
+using UnityEngine;
+
+public class TutorialText : MonoBehaviour
+{
+
+    [Header("Tutorial Text")]
+    public string header;
+    [TextArea(5,20)]
+    public string body;
+
+    public TMP_Text headerText;
+    public TMP_Text bodyText;
+    
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        headerText.text = header;
+        bodyText.text = body;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
