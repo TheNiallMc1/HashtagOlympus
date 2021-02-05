784883d5dcdc9cdd2ecf3f68efe1f3eb5ba26a89
Niall McCoy <niallmccoy123@outlook.com>
2021-02-05 12:59:52 +0000

    Tooltip polish and added to selection screen
    -Can assign text and BG colour to tooltips
    -No more flickering (had to make the tooltip avoid raycasts)
    -Mouse follow is smoother 
    -Anchoring is smoother
    -Changed fonts to fit UI style guide

--- OlympusUnity/Assets/_Project/Scripts/UIScripts/Tooltip/Tooltip.cs
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/Tooltip/Tooltip.cs
@@ -8,13 +8,19 @@ public class Tooltip : MonoBehaviour
     private PlayerControls playerControls;
 
     [HideInInspector] public Transform anchorPosition;
-    
+
+    public Image tooltipBackground;
     public TextMeshProUGUI headerField;
     public TextMeshProUGUI contentField;
     public LayoutElement layoutElement;
 
+    public Color32 headerColour = Color.white;
+    public Color32 contentColour = Color.white;
+    public Color32 backgroundColour = Color.black;
+
     private RectTransform rectTransform;
 
+    [Tooltip("How many characters can be displayed before the tooltip wraps to the next line")]
     public int characterWrapLimit;
 
     public void Start()
@@ -37,20 +43,20 @@ public class Tooltip : MonoBehaviour
         }
     }
 
-    private void MoveToAnchor()
+    public void MoveToAnchor()
     {
         transform.position = new Vector3(anchorPosition.position.x, anchorPosition.position.y, anchorPosition.position.z);
-        //
-        // float pivotX = transform.position.x / Screen.width;
-        // float pivotY = transform.position.y / Screen.height;
-        //
-        rectTransform.pivot = new Vector2(0, 1);
+        
+        float pivotX = transform.position.x / Screen.width;
+        float pivotY = transform.position.y / Screen.height;
+        
+        rectTransform.pivot = new Vector2(pivotX, pivotY);
     }
     
-    private void FollowMouse()
+    public void FollowMouse()
     {
         Vector3 mousePosition = GUIUtility.ScreenToGUIPoint(playerControls.Mouse.MousePos.ReadValue<Vector2>());
-        transform.position = new Vector3(mousePosition.x, mousePosition.y, mousePosition.z);
+        transform.position = new Vector3(mousePosition.x, mousePosition.y + 2, mousePosition.z);
 
         //transform.position = newPosition;
         
@@ -60,8 +66,17 @@ public class Tooltip : MonoBehaviour
         rectTransform.pivot = new Vector2(pivotX, pivotY);
     }
 
+    public void SetStyle()
+    {
+        headerField.color = headerColour;
+        contentField.color = contentColour;
+        tooltipBackground.color = backgroundColour;
+    }
+
     public void SetText(string content, string header)
     {
+        SetStyle();
+        
         if (string.IsNullOrEmpty(header))
         {
             headerField.gameObject.SetActive(false);
--- OlympusUnity/Assets/_Project/Scripts/UIScripts/Tooltip/TooltipSystem.cs
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/Tooltip/TooltipSystem.cs
@@ -11,17 +11,32 @@ public class TooltipSystem : MonoBehaviour
         current = this;
     }
 
-    public static void Show(string content, string header = "")
+    public static void Show(TooltipTrigger trigger, string content, string header = "")
     {
         current.tooltip.SetText(content, header);
+        
+        current.tooltip.backgroundColour = trigger.backgroundColour;
+        current.tooltip.contentColour = trigger.contentColour;
+        current.tooltip.headerColour = trigger.headerColour;
+        current.tooltip.SetStyle();
+
+        current.tooltip.FollowMouse();
         current.tooltip.gameObject.SetActive(true);
     }
     
-    public static void Show(Transform anchor, string content, string header = "")
+    public static void Show(TooltipTrigger trigger, Transform anchor, string content, string header = "")
     {
         current.tooltip.SetText(content, header);
-        current.tooltip.gameObject.SetActive(true);
+        
+        current.tooltip.backgroundColour = trigger.backgroundColour;
+        current.tooltip.contentColour = trigger.contentColour;
+        current.tooltip.headerColour = trigger.headerColour;
+        current.tooltip.SetStyle();
+        
         current.tooltip.anchorPosition = anchor;
+        
+        current.tooltip.MoveToAnchor();
+        current.tooltip.gameObject.SetActive(true);
     }
 
     public static void Hide()
