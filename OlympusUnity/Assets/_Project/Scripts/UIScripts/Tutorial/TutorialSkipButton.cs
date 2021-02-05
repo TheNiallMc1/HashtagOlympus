2a1f7f2ef5dec4a4da90beda552a7a1bf663c04b
bekah <caffeine@hotmail.co.uk>
2021-02-05 10:33:40 +0000

    added tutorial skip

---
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/Tutorial/TutorialSkipButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialSkipButton : MonoBehaviour
+{
+
+    public void SkipTutorial()
+    {
+        TutorialManager.Instance.mainTutorialComplete = true;
+    }
+}
