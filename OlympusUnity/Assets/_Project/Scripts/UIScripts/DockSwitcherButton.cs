b8d9a6da42e18085326390e58cd5bef191becf53
Niall McCoy <niallmccoy123@outlook.com>
2021-02-05 16:50:54 +0000

    Fixed errors so it builds now 😁

--- OlympusUnity/Assets/_Project/Scripts/UIScripts/DockSwitcherButton.cs
+++ OlympusUnity/Assets/_Project/Scripts/UIScripts/DockSwitcherButton.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using TMPro.EditorUtilities;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
 using UnityEngine.UI;
